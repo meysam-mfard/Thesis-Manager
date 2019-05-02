ab4e4cc4910b37365e9049f4f8291717b448b7b4
roblar91 <38360819+roblar91@users.noreply.github.com>
2019-05-02 16:35:11 +0200

    Added repositories and dummy model classes

--- thesismanager/src/main/java/e/group/thesismanager/model/Deadline.java
+++ thesismanager/src/main/java/e/group/thesismanager/model/Deadline.java
@@ -1,5 +1,12 @@
 package e.group.thesismanager.model;
 
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Data
 public class Deadline {
-    // TODO
+    private LocalDateTime projectDescriptionDeadline;
+    private LocalDateTime projectPlanDeadline;
+    private LocalDateTime projectReportDeadline;
+    private LocalDateTime projectFinalReportDeadline;
 }
