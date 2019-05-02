b4b8270822c24f2ce8259e2d0dfdbe3ae54d16b7
meysam-mfard <mmohagheghi@gmail.com>
2019-05-02 14:16:36 +0200

    Init project

---
+++ thesismanager/src/main/java/e/group/thesismanager/ThesismanagerApplication.java
@@ -0,0 +1,13 @@
+package e.group.thesismanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThesismanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThesismanagerApplication.class, args);
+    }
+
+}
