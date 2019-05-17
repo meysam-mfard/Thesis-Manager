4d12ca4e01d828c8983ec6f8f56a0e1438fb1e9e
AdellTat <38494534+AdellTat@users.noreply.github.com>
2019-05-17 19:34:09 +0200

    Opponent & Supervisor controllers + some neccessary edits to their services

--- thesismanager/src/main/java/e/group/thesismanager/repository/ThesisRepository.java
+++ thesismanager/src/main/java/e/group/thesismanager/repository/ThesisRepository.java
@@ -10,4 +10,6 @@ import java.util.List;
 public interface ThesisRepository extends JpaRepository<Thesis, Long> {
 
     List<Thesis> findThesesByStudentAndSemester(User user, Semester semester);
+    List<Thesis> findThesesByOpponent(User user);
+    List<Thesis> findThesesBySupervisorAndSupervisorAccept(User user, boolean answer);
 }
